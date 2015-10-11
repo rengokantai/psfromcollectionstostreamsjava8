31ed9b1993083d6d6169233e64703427f4d5ebc1
Hernan Y.Ke <yuriqiao@gmail.com>
2015-10-11 16:57:54 -0400

    cp5

---
+++ CP5/src/MainBuildingStreams.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Spliterator;
+import java.util.Spliterators;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.function.BinaryOperator;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+
+public class MainBuildingStreams {
+    
+    public static void main(String[] args) {
+
+        Stream.Builder<String> builder = Stream.builder();
+        builder.add("a").add("b");
+        Stream<String> st = builder.build();
+        st.forEach(System.out::println);
+        
+        List<Integer> ints = Arrays.asList(0, 1, 2, 3, 4);
+        
+        Stream<Integer> stream1 = ints.stream();
+        Stream<Integer> stream = Stream.of(0, 1, 2, 3, 4);
+        
+        stream.forEach(System.out::println);
+     
+        Stream<String> streamOfStrings = Stream.generate(() -> "one");
+        streamOfStrings.limit(5).forEach(System.out::println);
+        
+        Stream<String> streamOfStrings2 = Stream.iterate("+", s -> s + "+");
+        streamOfStrings2.limit(5).forEach(System.out::println);
+        
+        IntStream streamOfInt = ThreadLocalRandom.current().ints();
+        streamOfInt.limit(5).forEach(System.out::println);
+    }
+}
